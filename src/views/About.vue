c44afb5d162c409c6d1f53943ca927c1b02cd96d
Alex Owen <alex@ubqt.io>
2020-05-16 19:41:33 +0100

    about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About this project</h1>
+    <p>This is a code test in Vue.js, Vuex and tested with mocha and chai.</p>
+    <p>It was written using TDD, and should contain tests for all parts of all components.</p>
+    <p>It has these assumptions:</p>
+    <ul>
+      <li>Chart.js has been thoroughly tested, so does not need additional testing</li>
+      <li>The data from the json file is always correct</li>
+      <li>Data does not need to persist between reloads</li>
+      <li>There is a global stylesheet that would apply to everything here, it is a
+        demonstration of Vue, not of CSS</li>
+    </ul>
   </div>
 </template>
