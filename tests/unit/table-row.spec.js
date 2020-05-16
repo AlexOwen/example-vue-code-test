034626ccaef1f7d9ba8f8b75170b3a9d64b92db0
Alex Owen <alex@ubqt.io>
2020-05-16 19:05:25 +0100

    additonal unit tests

--- tests/unit/table-row.spec.js
+++ tests/unit/table-row.spec.js
@@ -55,4 +55,118 @@ describe('TableRow.vue', () => {
     expect(wrapper.find('.preferences_pet input').element.value).to.equal('dog');
     expect(wrapper.find('.preferences_fruit input').element.value).to.equal('papaya');
   });
+
+  it('can update the name', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        name: {
+          get() {
+            return 'test user';
+          },
+          set(value) {
+            expect(value).to.equal('new name');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.name input').setValue('new name');
+  });
+
+  it('can update the age', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        age: {
+          get() {
+            return '100';
+          },
+          set(value) {
+            expect(value).to.equal('18');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.age input').setValue('18');
+  });
+
+  it('can update the gender', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        gender: {
+          get() {
+            return 'unspecified';
+          },
+          set(value) {
+            expect(value).to.equal('female');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.gender input').setValue('female');
+  });
+
+  it('can update the eye color', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        eyeColor: {
+          get() {
+            return 'green';
+          },
+          set(value) {
+            expect(value).to.equal('black');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.eyecolor input').setValue('black');
+  });
+
+  it('can update the preferred pet', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        pet: {
+          get() {
+            return 'dog';
+          },
+          set(value) {
+            expect(value).to.equal('none');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.preferences_pet input').setValue('none');
+  });
+
+  it('can update the preferred fruit', () => {
+    const wrapper = shallowMount(TableRow, {
+      computed: {
+        ...computed,
+        fruit: {
+          get() {
+            return 'grapes';
+          },
+          set(value) {
+            expect(value).to.equal('orange');
+          },
+        },
+      },
+      propsData: { personIndex: 0 },
+    });
+
+    wrapper.find('.preferences_fruit input').setValue('orange');
+  });
 });
