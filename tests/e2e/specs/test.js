5303607652b10b99ca8a9ec2a15702fd3f1b55a6
Alex Owen <alex@ubqt.io>
2020-05-16 20:48:44 +0100

    e2e testing update

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,27 +1,8 @@
 // For authoring Nightwatch tests, see
 // https://nightwatchjs.org/guide
 
-// module.exports = {
-//   'default e2e tests': (browser) => {
-//     browser
-//       .init()
-//       .waitForElementVisible('#app')
-//       .assert.elementPresent('.hello')
-//       .assert.containsText('h1', 'Welcome to Your Vue.js App')
-//       .assert.elementCount('img', 1)
-//       .end();
-//   },
-
-//   'example e2e test using a custom command': (browser) => {
-//     browser
-//       .openHomepage()
-//       .assert.elementPresent('.hello')
-//       .end();
-//   },
-// };
-
 module.exports = {
-  'title exists': browser => {
+  'title exists': (browser) => {
     browser
       .init()
       .waitForElementVisible('#app')
@@ -29,11 +10,25 @@ module.exports = {
       .assert.containsText('h1', 'Vue.js Code Test')
       .end();
   },
-
-  // 'example e2e test using a custom command': (browser) => {
-  //   browser
-  //     .openHomepage()
-  //     .assert.elementPresent('.hello')
-  //     .end();
-  // },
+  'bar chart exists': (browser) => {
+    browser
+      .init()
+      .waitForElementVisible('#app')
+      .assert.elementPresent('canvas#bar-chart')
+      .end();
+  },
+  'pie chart exists': (browser) => {
+    browser
+      .init()
+      .waitForElementVisible('#app')
+      .assert.elementPresent('canvas#pie-chart')
+      .end();
+  },
+  'table exists': (browser) => {
+    browser
+      .init()
+      .waitForElementVisible('#app')
+      .assert.elementPresent('table')
+      .end();
+  },
 };
